28c5de6677ecafec97f4483e404427d0ae174d71
daniel <daniel.oliveira.unip@gmail.com>
2023-05-25 11:37:58 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"NOVO HOMEM DE FERRO EM 3D",
+    "description":"marvel anuncia um novo filme do homem de ferro, confira",
+    "photoCover":"'https://uploads.jovemnerd.com.br/wp-content/uploads/2022/07/tony_stark_vingadores_ultimato_cena__70kqp20-1210x544.jpg"
+  }
+,
+  {
+    "id":"2",
+    "title":"Nova Série anunciada n Disney +",
+    "description":"bla blabla",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  }
+]
